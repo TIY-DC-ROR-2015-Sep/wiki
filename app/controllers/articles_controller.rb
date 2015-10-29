1e408ef90dc1277f62080c263b364f9915d12383
James Dabbs <jamesdabbs@gmail.com>
2015-10-29 11:26:39 -0400

    Working article update ui

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,23 @@
 class ArticlesController < ApplicationController
   def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find params[:id]
+  end
+
+  def edit
+    @article = Article.find params[:id]
+  end
+
+  def update
+    @article = Article.find params[:id]
+    begin
+      current_user.edit @article, params[:article][:body]
+      redirect_to @article, notice: "Article saved"
+    rescue ActiveRecord::RecordInvalid
+      render :edit
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  resources :articles, except: [:delete]
+
   root to: 'articles#index'
 end
